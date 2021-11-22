caf5a14bd9e79f95bcba353bb0a823ee78a2fc96
Muhammad Alif Akbar <90602823+alifakbar46@users.noreply.github.com>
2021-11-22 20:41:47 +0700

    Add files via upload

--- Buku.php
+++ Buku.php
@@ -41,4 +41,32 @@ class Buku extends CI_Controller
         $this->ModelBuku->hapusKategori($where);
         redirect('buku/kategori');
     }
+
+    public function ubahKategori()
+    {
+        $data['judul'] = 'Ubah Data Kategori';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+        $data['kategori'] = $this->ModelBuku->Kategoriwhere(['id' =>
+        $this->uri->segment(3)])->result_array();
+
+        $this->form_validation->set_rules('kategori', 'Nama Kategori', '
+             required|min_length[3]', [
+            'required' => 'Nama Kategori harus diisi',
+            'min_length' => 'Nama Kategori terlalu pendek'
+        ]);
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('buku/ubah_kategori', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $data = [
+                'kategori' => $this->input->post('kategori', true)
+            ];
+            $this->ModelBuku->updateKategori(['id' => $this->input->post('id')], $data);
+            redirect('buku/kategori');
+        }
+    }
 }
